b9eff1f914c379d923d5c8a75e4a4181bcedfce7
Aanvig <78943357+Aanvig@users.noreply.github.com>
2021-10-20 20:41:13 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("H I N T  1", 100,50)
+    fill("lightblue")
+    text("What walks on four legs in the morning . . .", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("H I N T  2", 700,150)
+    fill("lightblue")
+    text(". . . on two legs during the day . . .", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("H I N T  3", 100,250)
+    fill("lightblue")
+    text(". . . and three legs at night?", 100,270)
+
+}
---
+++ js/System.js
@@ -0,0 +1,17 @@
+class System{
+
+    constructor(){}
+
+    authenticate(People, enteredCode) {
+        if(People === enteredCode) 
+            return true
+        else
+            return false
+        
+    }
+
+    // Add code to authenticate the given code and the access codes.
+
+    
+
+}
